3e139702b12612a21052297853ada21ce0ba6a67
Caio Vinícius <caio1525pereira@gmail.com>
2022-12-14 21:02:16 -0300

    refactor: :bricks: Refatoração na entidade Notification

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -1,3 +1,5 @@
+import { Replace } from "src/helpers/Replace";
+
 import { Content } from "./content";
 
 export interface NotificationProps {
@@ -11,8 +13,11 @@ export interface NotificationProps {
 export class Notification {
   private props: NotificationProps;
 
-  constructor(props: NotificationProps) {
-    this.props = props;
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date()
+    };
   }
 
   public set recipientId(recipientId: string) {
@@ -23,11 +28,11 @@ export class Notification {
     return this.props.recipientId;
   }
 
-  public set content(content: string) {
-    this.props.content = new Content(content);
+  public set content(content: Content) {
+    this.props.content = content;
   }
 
-  public get content(): Content | any {
+  public get content(): Content {
     return this.props.content;
   }
 
