4326d6e27e0d9f6a37e7130b255c923486a2df08
Caio Vinícius <caio1525pereira@gmail.com>
2022-12-16 21:40:02 -0300

    test: :white_check_mark: Atualização dos testes do caso de uso de envio de notificação

--- src/app/useCases/sendNotification.spec.ts
+++ src/app/useCases/sendNotification.spec.ts
@@ -1,8 +1,15 @@
+import { NotificationsRepositoryInMemory } from "../../../test/repositories/NotificationsRepositoryInMemory";
 import { SendNotification } from "./sendNotification";
 
+let notificationsRepository: NotificationsRepositoryInMemory;
+
 describe("Send notification", () => {
+  beforeEach(() => {
+    notificationsRepository = new NotificationsRepositoryInMemory();
+  });
+
   it("should be able to send a notification", async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
     const { notification } = await sendNotification.execute({
       content: "Você tem uma nova solicitação de amizade",
@@ -10,6 +17,7 @@ describe("Send notification", () => {
       recipientId: "ad585a3c-250a-4cea-abe2-21c4f1a1dc1f"
     });
 
-    expect(notification).toBeTruthy();
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
