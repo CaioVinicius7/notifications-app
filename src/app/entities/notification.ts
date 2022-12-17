ef21b8cc3684077fc2fa0680ebeb5028785f4495
Caio Vinícius <caio1525pereira@gmail.com>
2022-12-17 16:21:00 -0300

    feat: :bricks: Mudanças na entidade Notification

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -1,5 +1,6 @@
-import { Replace } from "src/helpers/Replace";
+import { randomUUID } from "node:crypto";
 
+import { Replace } from "../../helpers/Replace";
 import { Content } from "./content";
 
 export interface NotificationProps {
@@ -11,13 +12,19 @@ export interface NotificationProps {
 }
 
 export class Notification {
+  private _id: string;
   private props: NotificationProps;
 
   constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this._id = randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date()
     };
+  }
+
+  public get id(): string {
+    return this._id;
   }
 
   public set recipientId(recipientId: string) {
