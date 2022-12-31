14abc1f5519eab969deb72d1e6d2e82d16babe34
Caio Vinícius <caio1525pereira@gmail.com>
2022-12-31 19:40:46 -0300

    feat: :sparkles: Consumer de envio de notificação

--- src/infra/messaging/messaging.module.ts
+++ src/infra/messaging/messaging.module.ts
@@ -1,10 +1,13 @@
+import { SendNotification } from "@app/useCases/sendNotification";
+import { DatabaseModule } from "@infra/database/database.module";
 import { Module } from "@nestjs/common";
 
+import { NotificationsController } from "./kafka/controllers/notifications.controller";
 import { KafkaConsumerService } from "./kafka/kafkaConsumer.service";
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [KafkaConsumerService]
+  imports: [DatabaseModule],
+  controllers: [NotificationsController],
+  providers: [KafkaConsumerService, SendNotification]
 })
 export class MessagingModule {}
