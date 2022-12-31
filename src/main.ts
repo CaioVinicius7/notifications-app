02ff3baf35fd8ed64abb54c435ec09e7367f8435
Caio Vinícius <caio1525pereira@gmail.com>
2022-12-31 19:52:40 -0300

    docs: :wrench: Configuração inicial do swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { KafkaConsumerService } from "@infra/messaging/kafka/kafkaConsumer.service";
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import { MicroserviceOptions } from "@nestjs/microservices/interfaces";
+import { MicroserviceOptions } from "@nestjs/microservices";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
 
@@ -23,6 +24,24 @@ async function bootstrap() {
   });
 
   await app.startAllMicroservices();
+
+  const config = new DocumentBuilder()
+    .setTitle("Notifications microservices")
+    .setDescription(
+      "Micro serviço de notificações desenvolvido durante o evento Ignite lab da Rocketseat"
+    )
+    .setVersion("1.0")
+    .addTag("Notifications")
+    .setContact(
+      "Caio Vinícius",
+      "https://github.com/caiovinicius7",
+      "caio1525pereira@gmail.com"
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("api-docs", app, document);
 
   await app.listen(3333);
 }
