36aa6366d0f0180f62970d64887103decb9c808f
Caio Vinícius <caio1525pereira@gmail.com>
2022-12-31 21:12:33 -0300

    feat: :goal_net: Utilização do NotFoundException do nest  na classe tratativa de erros NotificationNotFound

--- src/app/useCases/errors/notificationNotFound.ts
+++ src/app/useCases/errors/notificationNotFound.ts
@@ -1,4 +1,6 @@
-export class NotificationNotFound extends Error {
+import { NotFoundException } from "@nestjs/common/exceptions/not-found.exception";
+
+export class NotificationNotFound extends NotFoundException {
   constructor() {
     super("Notification not found.");
   }
